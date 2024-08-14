b08f5b69f68ec2de14ba653517f72246415d38ed
Christian Hugo Sánchez Quispilaya <chsanchezq@gmail.com>
2024-08-13 20:21:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-import { IfcViewerAPI } from 'web-ifc-viewer';
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('viewer');
+    const viewer = new IfcViewerAPI({ container, backgroundColor: 0xaaaaaa });
+    viewer.grid.setGrid();
+    viewer.axes.setAxes();
 
-// Configuración básica de la escena
-const container = document.getElementById('viewer');
-const viewer = new IfcViewerAPI({ container, backgroundColor: 0xaaaaaa });
-viewer.grid.setGrid();
-viewer.axes.setAxes();
+    async function loadIfc(url) {
+        try {
+            await viewer.IFC.loadIfcUrl(url);
+            viewer.camera.fitModel();
+        } catch (error) {
+            console.error('Error al cargar el archivo IFC:', error);
+        }
+    }
 
-// Cargar archivo IFC
-async function loadIfc(url) {
-    await viewer.IFC.loadIfcUrl(url);
-    viewer.camera.fitModel();
-}
-
-// Llama a la función con la URL de tu archivo IFC
-loadIfc('https://drive.google.com/uc?export=download&id=1L7Ba6i7emG31HL-tyXCkZpzeU__umQSW');
+    loadIfc('https://drive.google.com/uc?export=download&id=1L7Ba6i7emG31HL-tyXCkZpzeU__umQSW');
+});
