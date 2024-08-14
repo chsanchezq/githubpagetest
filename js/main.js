9a46393c4601e45adf55a4eb21192b07076ec481
Christian Hugo Sánchez Quispilaya <chsanchezq@gmail.com>
2024-08-13 19:53:46 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+// Configuración básica de la escena
+const container = document.getElementById('viewer');
+const viewer = new IfcViewerAPI({ container, backgroundColor: 0xaaaaaa });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+// Cargar archivo IFC
+async function loadIfc(url) {
+    await viewer.IFC.loadIfcUrl(url);
+    viewer.camera.fitModel();
+}
+
+// Llama a la función con la URL de tu archivo IFC
+loadIfc('assets/rac_basic_sample_project.ifc');
